489b3392b6fe9d574fdb54a07fa21b3c80cb04f0
algnot <thanawat.k2000@gmail.com>
2023-02-08 00:14:01 +0700

    add unit test

---
+++ test/app.test.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const { setupStrapi, cleanupStrapi } = require("./helpers/strapi");
+
+jest.setTimeout(1000 * 60 * 60);
+
+beforeAll(async () => {
+  await setupStrapi();
+});
+
+afterAll(async () => {
+  await cleanupStrapi();
+});
+
+it("strapi is defined", () => {
+  expect(strapi).toBeDefined();
+});
