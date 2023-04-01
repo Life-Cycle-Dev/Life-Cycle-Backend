63aeebac85910ebf6faa60fd22011d5ac7d58ed5
algnot <thanawat.k2000@gmail.com>
2023-04-01 12:04:55 +0700

    fix

---
+++ src/api/sleep-cycle-line/controllers/sleep-cycle-line.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sleep-cycle-line controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::sleep-cycle-line.sleep-cycle-line');
---
+++ src/api/sleep-cycle-line/services/sleep-cycle-line.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sleep-cycle-line service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::sleep-cycle-line.sleep-cycle-line');
